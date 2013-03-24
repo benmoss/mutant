e4a36bbe30f0bcbb77bc85e3427304657feb30b7
Dan Kubb <dan.kubb@gmail.com>
2013-03-24 16:33:39 -0700

    Add coverage testing to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,26 @@
 require 'devtools/spec_helper'
 
 $: << File.join(TestApp.root,'lib')
-
 require 'test_app'
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    command_name     'spec:unit'
+    add_filter       'config'
+    add_filter       'spec'
+    add_filter       'test_app'
+    minimum_coverage 81.87
+  end
+end
+
 require 'mutant'
 
 RSpec.configure do |config|
